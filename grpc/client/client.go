53d18986856a7ea5dc92a87989048fa8ed150edb
Harri Lainio <harri.lainio@op.fi>
2020-11-12 19:55:31 +0200

    helper client for gRPC is multi-tenant to support client connections to server with many different CA DID, mainly needed for the tests

--- grpc/client/client.go
+++ grpc/client/client.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 
@@ -15,16 +16,19 @@ import (
 	"google.golang.org/grpc"
 )
 
-var Conn *grpc.ClientConn
-var cfg *rpc.ClientCfg
+type Conn struct {
+	*grpc.ClientConn
+	cfg *rpc.ClientCfg
+}
 
 type Pairwise struct {
+	Conn
 	ID    string
 	Label string
 }
 
 func BuildClientConnBase(tlsPath, addr string, port int, opts []grpc.DialOption) *rpc.ClientCfg {
-	cfg = &rpc.ClientCfg{
+	cfg := &rpc.ClientCfg{
 		PKI:  rpc.LoadPKI(tlsPath),
 		JWT:  "",
 		Addr: fmt.Sprintf("%s:%d", addr, port),
@@ -33,45 +37,19 @@ func BuildClientConnBase(tlsPath, addr string, port int, opts []grpc.DialOption)
 	return cfg
 }
 
-func TryOpen(user string, conf *rpc.ClientCfg) *grpc.ClientConn {
+func TryOpen(user string, conf *rpc.ClientCfg) (c Conn) {
 	if conf == nil {
-		conf = cfg
+		panic(errors.New("conf cannot be nil"))
 	}
 	glog.V(1).Infof("client with user \"%s\"", user)
 	conf.JWT = jwt.BuildJWT(user)
 	conn, err := rpc.ClientConn(*conf)
 	err2.Check(err)
-	Conn = conn
-	return conn
+	return Conn{ClientConn: conn, cfg: conf}
 }
 
 func OkStatus(s *agency.ProtocolState) bool {
 	return s.State == agency.ProtocolState_OK
-}
-
-func TryOpenConnDeprecated(user, addr string, port int, opts []grpc.DialOption) *grpc.ClientConn {
-	conn, err := OpenClientConnDeprecated(user, fmt.Sprintf("%s:%d", addr, port), opts)
-	err2.Check(err)
-	return conn
-}
-
-func OpenClientConnDeprecated(user, addr string, opts []grpc.DialOption) (conn *grpc.ClientConn, err error) {
-	defer err2.Return(&err)
-
-	//if Conn != nil {
-	//	return nil, errors.New("client connection all ready open")
-	//}
-	pki := rpc.LoadPKI("")
-	glog.V(5).Infof("client with user \"%s\"", user)
-	conn, err = rpc.ClientConn(rpc.ClientCfg{
-		PKI:  pki,
-		JWT:  jwt.BuildJWT(user),
-		Addr: addr,
-		Opts: opts,
-	})
-	err2.Check(err)
-	Conn = conn
-	return
 }
 
 func (pw Pairwise) Issue(ctx context.Context, credDefID, attrsJSON string) (ch chan *agency.ProtocolState, err error) {
@@ -84,7 +62,7 @@ func (pw Pairwise) Issue(ctx context.Context, credDefID, attrsJSON string) (ch c
 			Attrs:     &agency.Protocol_Issuing_AttributesJson{AttributesJson: attrsJSON},
 		}},
 	}
-	return doStart(ctx, protocol)
+	return pw.Conn.doStart(ctx, protocol)
 }
 
 func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *agency.Protocol_Attrs) (ch chan *agency.ProtocolState, err error) {
@@ -97,10 +75,10 @@ func (pw Pairwise) IssueWithAttrs(ctx context.Context, credDefID string, attrs *
 			Attrs:     &agency.Protocol_Issuing_Attrs_{Attrs_: attrs},
 		}},
 	}
-	return doStart(ctx, protocol)
+	return pw.Conn.doStart(ctx, protocol)
 }
 
-func Connection(ctx context.Context, invitationJSON string) (connID string, ch chan *agency.ProtocolState, err error) {
+func (conn Conn) Connection(ctx context.Context, invitationJSON string) (connID string, ch chan *agency.ProtocolState, err error) {
 	defer err2.Return(&err)
 
 	// assert that invitation is OK, and we need to return the connection ID
@@ -113,7 +91,7 @@ func Connection(ctx context.Context, invitationJSON string) (connID string, ch c
 		Role:     agency.Protocol_INITIATOR,
 		StartMsg: &agency.Protocol_InvitationJson{InvitationJson: invitationJSON},
 	}
-	ch, err = doStart(ctx, protocol)
+	ch, err = conn.doStart(ctx, protocol)
 	err2.Check(err)
 	connID = invitation.ID
 	return connID, ch, err
@@ -125,7 +103,7 @@ func (pw Pairwise) Ping(ctx context.Context) (ch chan *agency.ProtocolState, err
 		TypeId:       agency.Protocol_TRUST_PING,
 		Role:         agency.Protocol_INITIATOR,
 	}
-	return doStart(ctx, protocol)
+	return pw.Conn.doStart(ctx, protocol)
 }
 
 func (pw Pairwise) BasicMessage(ctx context.Context, content string) (ch chan *agency.ProtocolState, err error) {
@@ -135,7 +113,7 @@ func (pw Pairwise) BasicMessage(ctx context.Context, content string) (ch chan *a
 		Role:         agency.Protocol_INITIATOR,
 		StartMsg:     &agency.Protocol_BasicMessage{BasicMessage: content},
 	}
-	return doStart(ctx, protocol)
+	return pw.Conn.doStart(ctx, protocol)
 }
 
 func (pw Pairwise) ReqProof(ctx context.Context, proofAttrs string) (ch chan *agency.ProtocolState, err error) {
@@ -148,7 +126,7 @@ func (pw Pairwise) ReqProof(ctx context.Context, proofAttrs string) (ch chan *ag
 				AttrFmt: &agency.Protocol_ProofRequest_AttributesJson{
 					AttributesJson: proofAttrs}}},
 	}
-	return doStart(ctx, protocol)
+	return pw.Conn.doStart(ctx, protocol)
 }
 
 func (pw Pairwise) ReqProofWithAttrs(ctx context.Context, proofAttrs *agency.Protocol_Proof) (ch chan *agency.ProtocolState, err error) {
@@ -161,13 +139,13 @@ func (pw Pairwise) ReqProofWithAttrs(ctx context.Context, proofAttrs *agency.Pro
 				AttrFmt: &agency.Protocol_ProofRequest_Attrs{
 					Attrs: proofAttrs}}},
 	}
-	return doStart(ctx, protocol)
+	return pw.Conn.doStart(ctx, protocol)
 }
 
-func Listen(ctx context.Context, protocol *agency.ClientID) (ch chan *agency.AgentStatus, err error) {
+func (conn Conn) Listen(ctx context.Context, protocol *agency.ClientID) (ch chan *agency.AgentStatus, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewAgentClient(Conn)
+	c := agency.NewAgentClient(conn)
 	statusCh := make(chan *agency.AgentStatus)
 
 	stream, err := c.Listen(ctx, protocol)
@@ -192,10 +170,10 @@ func Listen(ctx context.Context, protocol *agency.ClientID) (ch chan *agency.Age
 	return statusCh, nil
 }
 
-func doStart(ctx context.Context, protocol *agency.Protocol) (ch chan *agency.ProtocolState, err error) {
+func (conn Conn) doStart(ctx context.Context, protocol *agency.Protocol) (ch chan *agency.ProtocolState, err error) {
 	defer err2.Return(&err)
 
-	c := agency.NewDIDCommClient(Conn)
+	c := agency.NewDIDCommClient(conn)
 	statusCh := make(chan *agency.ProtocolState)
 
 	stream, err := c.Run(ctx, protocol)
