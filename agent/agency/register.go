e3520fc2777a68c5a7773b49dc1e7d14139bbc3e
Harri Lainio <harri.lainio@op.fi>
2021-02-06 18:57:04 +0200

    first version of automatic backup of cloud agent registry

--- agent/agency/register.go
+++ agent/agency/register.go
@@ -2,13 +2,21 @@ package agency
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"time"
 
 	"github.com/findy-network/findy-agent/agent/utils"
 	"github.com/golang/glog"
+	"github.com/lainio/err2"
 )
 
-var Register utils.Reg // stores Agents already on-boarded, has Email as key
+var (
+	Register utils.Reg // stores Agents already on-boarded, has Email as key
+
+	lastBackup = time.Now()
+)
 
 func init() {
 	err := Register.Load("")
@@ -24,7 +32,34 @@ func SaveRegistered() {
 	err := Register.Save(utils.Settings.RegisterName())
 	if err != nil {
 		glog.Error(err)
+		return
 	}
+	if timeToBackup() {
+		// We leave file level sync handling for the OS for performance sake.
+		// We could add lambda to Register.Save function to perform backup in
+		// the same critical section as saving.
+		go backup()
+	}
+}
+
+func timeToBackup() bool {
+	interval := utils.Settings.RegisterBackupInterval()
+	// optimize, if backup is not set
+	if interval == 0 {
+		return false
+	}
+	return time.Now().Sub(lastBackup) >= interval
+}
+
+func backup() {
+	defer err2.CatchTrace(func(err error) {
+		glog.Warning(err)
+	})
+
+	name := backupName(utils.Settings.RegisterBackupName())
+	err2.Check(fileCopy(utils.Settings.RegisterName(), name))
+	glog.V(1).Infoln("CA register backup successful to:", name)
+	lastBackup = time.Now()
 }
 
 // ResetRegistered sets the correct filename for our persistent storage and
@@ -33,4 +68,26 @@ func ResetRegistered(filename string) error {
 	utils.Settings.SetRegisterName(filename)
 	fmt.Println("Note! Resetting handshake register, on-boarding starts over.")
 	return Register.Reset(filename)
+}
+
+func backupName(baseName string) string {
+	tsStr := time.Now().Format(time.RFC3339)
+	name := tsStr + "_" + baseName
+	glog.V(3).Infoln("backup name:", name)
+	return name
+}
+
+func fileCopy(src, dst string) (err error) {
+	defer err2.Returnf(&err, "copy %s %s", src, dst)
+
+	r := err2.File.Try(os.Open(src))
+	defer r.Close()
+
+	w := err2.File.Try(os.Create(dst))
+	defer err2.Handle(&err, func() {
+		os.Remove(dst)
+	})
+	defer w.Close()
+	err2.Empty.Try(io.Copy(w, r))
+	return nil
 }
