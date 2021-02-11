287b4cfd278cb0f3f0be51b0b936018c5f434029
Harri Lainio <harri.lainio@op.fi>
2021-02-11 16:14:50 +0200

    grpc admin id to base cmd

--- cmds/grpc.go
+++ cmds/grpc.go
@@ -6,11 +6,15 @@ type GrpcCmd struct {
 	TlsPath string
 	Addr    string
 	Port    int
+	AdminID string
 }
 
 func (c GrpcCmd) Validate() error {
 	if c.Addr == "" {
 		return errors.New("server address cannot be empty")
+	}
+	if c.AdminID == "" {
+		return errors.New("server admin id cannot be empty")
 	}
 	if c.Port == 0 {
 		return errors.New("server port cannot be zero")
