92cf9aa1e9aff03a0ca5faeb8e7035286b136baf
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-03-24 16:48:13 +0200

    Do not rebuild images with same tags.

--- agency/tools/push.sh
+++ agency/tools/push.sh
@@ -12,9 +12,23 @@ if [ -z "$FINDY_AGENCY_ECR_REPOSITORY" ]; then
   exit 1
 fi
 
+if [ -z "$FINDY_AGENCY_ECR_REPOSITORY_NAME" ]; then
+  echo "ERROR: Define env variable FINDY_AGENCY_ECR_REPOSITORY_NAME"
+  exit 1
+fi
+
 CURRENT_DIR=$(dirname "$BASH_SOURCE")
 
 VERSION=$(cat $CURRENT_DIR/../../VERSION)
+
+HAS_IMAGE_VERSION=$(aws ecr list-images --repository-name $FINDY_AGENT_ECR_REPOSITORY_NAME --filter '{"tagStatus": "TAGGED"}' | grep $VERSION)
+
+if [ -z "$HAS_IMAGE_VERSION" ]; then
+  echo "Image $VERSION not found in registry, starting building"
+else
+  echo "Image $VERSION already built, skipping"
+  exit 0
+fi
 
 echo "Releasing findy-agency version $VERSION"
 
