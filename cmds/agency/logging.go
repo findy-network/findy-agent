412ecc7462aa100aa5bbec1ae22f119dc5063a20
Harri Lainio <harri.lainio@op.fi>
2021-04-25 20:15:36 +0300

    idl compile results moved to findy-common-go

--- cmds/agency/logging.go
+++ cmds/agency/logging.go
@@ -5,9 +5,9 @@ import (
 	"io"
 	"time"
 
-	pb "github.com/findy-network/findy-agent-api/grpc/ops/v1"
 	"github.com/findy-network/findy-agent/cmds"
 	"github.com/findy-network/findy-common-go/agency/client"
+	pb "github.com/findy-network/findy-common-go/grpc/ops/v1"
 	"github.com/lainio/err2"
 )
 
--- cmds/agency/ping.go
+++ cmds/agency/ping.go
@@ -6,13 +6,13 @@ import (
 	"io"
 	"time"
 
-	pb "github.com/findy-network/findy-agent-api/grpc/ops/v1"
 	"github.com/findy-network/findy-agent/agent/agency"
 	"github.com/findy-network/findy-agent/agent/e2"
 	"github.com/findy-network/findy-agent/agent/endp"
 	"github.com/findy-network/findy-agent/agent/mesg"
 	"github.com/findy-network/findy-agent/cmds"
 	"github.com/findy-network/findy-common-go/agency/client"
+	pb "github.com/findy-network/findy-common-go/grpc/ops/v1"
 	"github.com/lainio/err2"
 )
 
