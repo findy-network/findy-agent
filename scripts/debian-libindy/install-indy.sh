51cb9cee9b182e66c845040b76affe5b412bdea3
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-06-08 16:04:33 +0300

    Use sudo in indy install script.

--- scripts/debian-libindy/install-indy.sh
+++ scripts/debian-libindy/install-indy.sh
@@ -3,12 +3,12 @@
 INDY_LIB_VERSION="1.16.0"
 UBUNTU_VERSION="bionic"
 
-apt-get update && \
-    apt-get install -y software-properties-common apt-transport-https && \
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68DB5E88 && \
-    add-apt-repository "deb https://repo.sovrin.org/sdk/deb $UBUNTU_VERSION stable" && \
-    add-apt-repository "deb https://repo.sovrin.org/sdk/deb xenial stable" && \
-    apt-get update
+sudo apt-get update && \
+    sudo apt-get install -y software-properties-common apt-transport-https && \
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 68DB5E88 && \
+    sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb $UBUNTU_VERSION stable" && \
+    sudo add-apt-repository "deb https://repo.sovrin.org/sdk/deb xenial stable" && \
+    sudo apt-get update
 
-apt-get install -y libindy-dev="$INDY_LIB_VERSION-xenial" \
+sudo apt-get install -y libindy-dev="$INDY_LIB_VERSION-xenial" \
     libindy="$INDY_LIB_VERSION-$UBUNTU_VERSION"
