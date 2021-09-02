ab94f92e607c90ec2ec156e0aa6a43cf31e48974
Laura Vuorenoja <laura.vuorenoja@op.fi>
2021-09-02 13:00:41 +0300

    Clean up prints while testing.

--- scripts/aath/record_logs.sh
+++ scripts/aath/record_logs.sh
@@ -3,7 +3,7 @@
 CONTAINER_NAME=$1
 
 container_starting(){
-    if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME )" == "true" ]; then
+    if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME 2>&1)" == "true" ]; then
         return 0
     else
         return 1
@@ -12,7 +12,7 @@ container_starting(){
 
 
 NOW=${SECONDS}
-printf "Wait until container is up\n"
+printf "Wait until container $CONTAINER_NAME is up\n"
 while ! container_starting; do
     waitTime=$(($SECONDS - $NOW))
     if (( ${waitTime} >= 600 )); then
@@ -22,5 +22,5 @@ while ! container_starting; do
     sleep 1
 done
 
-printf "Start saving logs"
+printf "Start saving logs for $CONTAINER_NAME"
 docker logs -f $CONTAINER_NAME > .logs/$CONTAINER_NAME-$(date +%s).log
