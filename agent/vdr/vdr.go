8fa461ce50903fc673d2c9ba9960da208e52a518
Harri Lainio <harri.lainio@op.fi>
2022-03-23 14:57:38 +0200

    using VDR thru api.AgentStorage interface

--- agent/vdr/vdr.go
+++ agent/vdr/vdr.go
@@ -1,9 +1,9 @@
 package vdr
 
 import (
-	"github.com/findy-network/findy-agent/agent/storage/mgddb"
+	"github.com/findy-network/findy-agent/agent/storage/api"
 	"github.com/hyperledger/aries-framework-go/pkg/framework/aries/api/vdr"
-	registry "github.com/hyperledger/aries-framework-go/pkg/vdr"
+	vdregistry "github.com/hyperledger/aries-framework-go/pkg/vdr"
 	"github.com/hyperledger/aries-framework-go/pkg/vdr/key"
 	"github.com/hyperledger/aries-framework-go/pkg/vdr/peer"
 	"github.com/lainio/err2"
@@ -23,7 +23,7 @@ type Config struct {
 	FilePath string
 }
 
-func New(storage *mgddb.Storage) (v *VDR, err error) {
+func New(storage api.AgentStorage) (v *VDR, err error) {
 	defer err2.Annotate("vdr new", &err)
 	v = &VDR{
 		keyVDR: &key.VDR{},
@@ -32,9 +32,9 @@ func New(storage *mgddb.Storage) (v *VDR, err error) {
 
 	v.peerVDR = peerVDR
 
-	v.registry = registry.New(
-		registry.WithVDR(v.keyVDR),
-		registry.WithVDR(v.peerVDR),
+	v.registry = vdregistry.New(
+		vdregistry.WithVDR(v.keyVDR),
+		vdregistry.WithVDR(v.peerVDR),
 	)
 
 	return v, nil
