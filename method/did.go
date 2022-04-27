4344289a83f257b5f98caaf6592bf45d94f2950c
Harri Lainio <harri.lainio@op.fi>
2022-04-27 17:51:40 +0300

    refactor for method.Type, adding TypePeer, reuse Base for Key&Peer impls

--- method/did.go
+++ method/did.go
@@ -7,11 +7,14 @@ import (
 	"github.com/findy-network/findy-agent/agent/service"
 	"github.com/findy-network/findy-agent/agent/storage/api"
 	"github.com/findy-network/findy-agent/core"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/did"
 	"github.com/hyperledger/aries-framework-go/pkg/kms"
 	"github.com/hyperledger/aries-framework-go/pkg/vdr/fingerprint"
+	"github.com/hyperledger/aries-framework-go/pkg/vdr/peer"
 	"github.com/lainio/err2"
 	"github.com/lainio/err2/assert"
 	"github.com/lainio/err2/try"
+	"github.com/mr-tron/base58"
 )
 
 func String(d string) string {
@@ -22,22 +25,44 @@ func String(d string) string {
 type Type int
 
 const (
-	TypeKey Type = 0 + iota
+	TypeUnknown Type = 0 + iota
+	TypeKey
 	TypePeer
 	TypeSov
 )
 
-func New(hStorage managed.Wallet, method Type) (id core.DID, err error) {
-	assert.D.True(method == TypeKey)
-	return NewKey(hStorage)
+func (t Type) String() string {
+	return []string{"unknown", "key", "peer", "sov"}[t]
 }
 
-type Key struct {
+func New(hStorage managed.Wallet, method Type) (id core.DID, err error) {
+	switch method {
+	case TypePeer:
+		return NewPeer(hStorage)
+	case TypeKey:
+		return NewKey(hStorage)
+	default:
+		assert.D.Truef(false, "did method (%v) not supported", method)
+	}
+	return
+}
+
+type Base struct {
 	kid string
 	pk  []byte
 	vkh any
 
+	doc *did.Doc
+
 	handle managed.Wallet
+}
+
+type Peer struct {
+	Base
+}
+
+type Key struct {
+	Base
 }
 
 func NewKey(hStorage managed.Wallet) (id core.DID, err error) {
@@ -47,7 +72,39 @@ func NewKey(hStorage managed.Wallet) (id core.DID, err error) {
 	kid, pk := try.To2(keys.CreateAndExportPubKeyBytes(kms.ED25519))
 	kh := try.To1(keys.PubKeyBytesToHandle(pk, kms.ED25519))
 
-	return Key{handle: hStorage, kid: kid, pk: pk, vkh: kh}, nil
+	return Key{Base{handle: hStorage, kid: kid, pk: pk, vkh: kh}}, nil
+}
+
+func NewPeer(hStorage managed.Wallet) (id core.DID, err error) {
+	defer err2.Annotate("new did:peer", &err)
+
+	keys := hStorage.Storage().KMS()
+	kid, pk := try.To2(keys.CreateAndExportPubKeyBytes(kms.ED25519))
+	kh := try.To1(keys.PubKeyBytesToHandle(pk, kms.ED25519))
+
+	key := did.VerificationMethod{
+		ID:         kid,
+		Type:       "Ed25519VerificationKey2018",
+		Controller: "",
+		Value:      pk,
+	}
+	doc := try.To1(peer.NewDoc(
+		[]did.VerificationMethod{key},
+		did.WithAuthentication([]did.Verification{{
+			VerificationMethod: key,
+			Relationship:       0,
+			Embedded:           true,
+		}}),
+		did.WithService([]did.Service{{
+			ID:              "didcomm",
+			Type:            "did-communication",
+			Priority:        0,
+			RecipientKeys:   []string{base58.Encode(pk)},
+			ServiceEndpoint: "http://example.com", // TODO: from where?
+		}}),
+	))
+
+	return Peer{Base{handle: hStorage, kid: kid, pk: pk, vkh: kh, doc: doc}}, nil
 }
 
 func NewKeyFromDID(
@@ -63,69 +120,110 @@ func NewKeyFromDID(
 	pk := try.To1(fingerprint.PubKeyFromDIDKey(didStr))
 	kh := try.To1(keys.PubKeyBytesToHandle(pk, kms.ED25519))
 
-	return Key{handle: hStorage, kid: "", pk: pk, vkh: kh}, nil
+	return Key{Base{handle: hStorage, kid: "", pk: pk, vkh: kh}}, nil
 }
 
-func (k Key) String() string {
+// String returns URI formated DID
+func (b Base) String() string {
 	// TODO: lazy fetch or move to constructor
-	didkey, _ := fingerprint.CreateDIDKey(k.pk)
+	didkey, _ := fingerprint.CreateDIDKey(b.pk)
 
 	return didkey
 }
 
-func (k Key) Did() string {
-	return k.kid
+func (b Base) Did() string {
+	return b.kid
 }
 
-func (k Key) KID() string {
-	return k.kid
+func (b Base) KID() string {
+	return b.kid
 }
 
-func (k Key) SignKey() any {
-	return k.vkh
+func (b Base) SignKey() any {
+	return b.vkh
 }
 
-func (k Key) Storage() managed.Wallet {
-	return k.handle
+func (b Base) Storage() managed.Wallet {
+	return b.handle
 }
 
-func (k Key) StartEndp(storageH managed.Wallet, connectionID string) {
+func (b Base) StartEndp(storageH managed.Wallet, connectionID string) {
 	// todo: check how this is implemented in ssi.DID.
 	// It seems that it's simple, but unnesseary.
 }
 
-func (k Key) Store(mgdWallet, mgdStorage managed.Wallet) {
+func (b Base) Store(mgdWallet, mgdStorage managed.Wallet) {
 	// todo: check the implementation from ssi.DID
 	// it seems that there is nothing to do, all is saved already.
 }
 
-func (k Key) SavePairwiseForDID(mStorage managed.Wallet, theirDID core.DID, pw core.PairwiseMeta) {
+func (b Base) SavePairwiseForDID(mStorage managed.Wallet, theirDID core.DID, pw core.PairwiseMeta) {
 	// todo: check ssi.DID, propably not needed
 }
 
-func (k Key) StoreResult() error {
+func (b Base) StoreResult() error {
 	// todo: see ssi.DID
 	return nil
 }
 
-func (k Key) AEndp() (ae service.Addr, err error) {
+func (b Base) AEndp() (ae service.Addr, err error) {
 	assert.D.NoImplementation()
 	return
 }
 
-func (k Key) SetAEndp(ae service.Addr) {
+func (b Base) SetAEndp(ae service.Addr) {
 	assert.D.NoImplementation()
 }
 
-func (k Key) Route() []string {
+func (b Base) Route() []string {
 	return []string{}
 }
 
 // TODO: this is mainly for indy but could be merged with SignKey?
-func (k Key) VerKey() string {
-	return string(k.pk)
+func (b Base) VerKey() string {
+	return string(b.pk)
 }
 
-func (k Key) Packager() api.Packager {
-	return k.handle.Storage().OurPackager()
+func (b Base) Packager() api.Packager {
+	return b.handle.Storage().OurPackager()
+}
+
+func (b Base) DOC() core.DIDDoc {
+	return b.doc
+}
+
+// newPeerDID is copied from framework's tests to find smallest common divider
+// to create `did:peer` with only one dependcy which is here kms.KeyManager.
+func newPeerDID(keys kms.KeyManager) (d *did.Doc, err error) {
+	defer err2.Return(&err)
+
+	// main() catch all err2 errors
+
+	kid, pubKey, err := keys.CreateAndExportPubKeyBytes(kms.ED25519)
+	err2.Check(err)
+
+	key := did.VerificationMethod{
+		ID:         kid,
+		Type:       "Ed25519VerificationKey2018",
+		Controller: "",
+		Value:      pubKey,
+	}
+	doc, err := peer.NewDoc(
+		[]did.VerificationMethod{key},
+		did.WithAuthentication([]did.Verification{{
+			VerificationMethod: key,
+			Relationship:       0,
+			Embedded:           true,
+		}}),
+		did.WithService([]did.Service{{
+			ID:              "didcomm",
+			Type:            "did-communication",
+			Priority:        0,
+			RecipientKeys:   []string{base58.Encode(pubKey)},
+			ServiceEndpoint: "http://example.com",
+		}}),
+	)
+	err2.Check(err)
+
+	return doc, nil
 }
