d82e4b87b2536277b4310325f2719b09af24f477
Harri Lainio <harri.lainio@op.fi>
2022-05-05 18:10:23 +0300

    connection.ToJSON removed unnecessary

--- std/didexchange/conn_impl.go
+++ std/didexchange/conn_impl.go
@@ -1,15 +1,1 @@
 package didexchange
-
-import (
-	"github.com/lainio/err2"
-)
-
-func (c *Connection) ToJSON() (b []byte, err error) {
-	defer err2.Return(&err)
-
-	return nil, nil
-
-	//	c.DIDDoc = try.To1(c.Doc.JSONBytes())
-
-	//	return json.Marshal(c)
-}
