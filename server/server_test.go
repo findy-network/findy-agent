9c59a25dec75f0cc65ac9201b22fbc1b78633cff
Laura Vuorenoja <laura.vuorenoja@op.fi>
2022-08-09 13:59:15 +0300

    Add test for ready handler.

---
+++ server/server_test.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/findy-network/findy-agent/agent/agency"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotReadyHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/ready", nil)
+	w := httptest.NewRecorder()
+
+	checkReady(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+	assert.Equal(t, "Not ready", string(data))
+	assert.Equal(t, http.StatusServiceUnavailable, res.StatusCode)
+}
+
+func TestReadyHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/ready", nil)
+	w := httptest.NewRecorder()
+
+	agency.Ready.RegisteringComplete()
+	checkReady(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+	assert.Equal(t, "OK ready", string(data))
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+}
